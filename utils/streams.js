a86f1791894a0e7611e3e63565ad8c4122922784
Александр Брюзгин < >
2017-10-03 23:02:24 +0300

    Colorful fun

--- utils/streams.js
+++ utils/streams.js
@@ -1,11 +1,12 @@
 import minimist from "minimist";
+import colors from 'colors'; // just for fun =)
 import config from "./../config";
 
-const helpText = "This tool will help you to run some stream utilities.\n" + 
-                "Just provide next options when calling a file to run your operation:\n\n" +
-                "--action, -a       Put here a method you want to call\n" + 
+const helpText = ("This tool will help you to run some stream utilities.\n" + 
+                "Just provide next options when calling a file to run your operation:\n\n").cyan +
+                ("--action, -a       Put here a method you want to call\n" + 
                 "--file, -f         Provide here a file you want to be operated with action\n" + 
-                "--help, -h         Call me for help =)";
+                "--help, -h         Call me for help =)").rainbow;
 const args = minimist(process.argv.slice(2), config.streamsMinimistOpts);
 const argsKeys = Object.keys(args);
 argsKeys.forEach((arg, i) => {
