4422fb351a959f5942214f5baa51fe531ae53bae
Александр Брюзгин < >
2017-10-15 22:57:40 +0300

    Added css bundler(not the end)

--- utils/streams.js
+++ utils/streams.js
@@ -1,6 +1,8 @@
 import minimist from "minimist";
 import fs from "fs";
 import path from "path";
+import http from "http";
+import { promisify } from "util";
 import through2 from "through2";
 import colors from 'colors'; // just for fun =)
 import config from "./../config";
@@ -41,16 +43,18 @@ const csvToJSONOut = (fileName) => {
     .pipe(process.stdout);
 }
 
+// task 7
 const csvToJSONFileNames = ["csvToJSONFile", , "csvJSONFile"];
 const csvToJSONFile = (fileName) => {
   const extName = path.extname(fileName);
   if (extName.toLowerCase() !== '.csv') return ;
 
   const baseName = path.basename(fileName, extName);
-  const writer = fs.createWriteStream(`${baseName}.json`);
+  const pathName = path.dirname(fileName);
+  const writer = fs.createWriteStream(`${pathName}/${baseName}.json`);
 
   writer.on("finish", () => console.info(`${baseName}.json was created`.bgRed.bgYellow));
-  
+
   fs.createReadStream(fileName)
     .pipe(through2(function (chunk, enc, cb) {
       const str = chunk.toString();
@@ -62,11 +66,34 @@ const csvToJSONFile = (fileName) => {
     .pipe(writer);
 }
 
+// task 8
+const readdir = promisify(fs.readdir);
+const httpGet = promisify(http.get);
+const cssBundleNames = ["cssBundle", "cssBundler", "css", "bundler"];
+const cssBundle = (dirname) => {
+  const writer = fs.createWriteStream(`${dirname}/bundle.css`);
+  readdir(dirname)
+    .then((files) => {
+      files.map((file) => {
+        fs.createReadStream(`${dirname}/${file}`)
+          .pipe(through2(function(chunk, enc, cb) {
+            const css = chunk.toString() + "\n";
+            this.push(css);
+            cb();
+          }))
+          // .pipe(process.stdout)
+          .pipe(fs.createWriteStream(`${dirname}/bundle.css`));
+      });
+    })
+    .catch(err => console.log(2));
+}
+
 const helpText = ("This tool will help you to run some stream utilities.\n" + 
                 "Just provide next options when calling a file to run your operation:\n\n").cyan +
                 ("--action, -a      Put here a method you want to call\n" + 
                 "--file, -f         Provide here a file you want to be operated with action\n" + 
-                "--help, -h         Call me for help =)").rainbow;
+                "--path, -p         For CSS bundling only; path to CSS files to concatenate\n" + 
+                "--help, -h         Call me for help =)\n").rainbow;
 
 const args = minimist(process.argv.slice(2), config.streamsMinimistOpts);
 const argsKeys = Object.keys(args);
@@ -75,6 +102,10 @@ argsKeys.forEach((arg, i) => {
     console.info(helpText);
   }
 });
+
+if (argsKeys.length < 2) {
+  console.info('No args found. If you donno what to do, add a "-h" or "--help" flag');
+}
 
 if(extractFileDataNames.includes(args.a) && args.f) {
   extractFileData(args.f);
@@ -92,6 +123,6 @@ if(csvToJSONFileNames.includes(args.a) && args.f) {
   csvToJSONFile(args.f);
 }
 
-if (argsKeys.length < 2) {
-  console.info('No args found. If you donno what to do, add a "-h" or "--help" flag');
+if(cssBundleNames.includes(args.a) && args.p) {
+  cssBundle(args.p);
 }
