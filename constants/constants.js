bc7f4d188e4d24c265398a45e79ef3065b8ad9ac
Aliaksandr Bruzgin <Aliaksandr_Bruzgin@epam.com>
2017-10-18 09:36:42 +0300

    Fixes according to PR comments

---
+++ constants/constants.js
@@ -0,0 +1,8 @@
+export const streamsCssUrl = "https://www.epam.com/etc/clientlibs/foundation/main.min.fc69c13add6eae57cd247a91c7e26a15.css";
+export const streamsCssBundleFile = "bundle.css";
+export const streamsHelpText = `This tool will help you to run some stream utilities.
+Just provide next options when calling a file to run your operation:\n
+--action, -a       Put here a method you want to call
+--file, -f         Provide here a file you want to be operated with action 
+--path, -p         For CSS bundling only; path to CSS files to concatenate
+--help, -h         Call me for help =)\n`
--- utils/streams.js
+++ utils/streams.js
@@ -4,13 +4,16 @@ import https from "https";
 import { promisify } from "util";
 import minimist from "minimist";
 import through2 from "through2";
-import colors from 'colors'; // just for fun =)
+import colors from 'colors';
 import config from "./../config";
+import { streamsCssUrl, streamsCssBundleFile, streamsHelpText } from "../constants/constants";
 import { csvToJSON } from './';
+
+const readdir = promisify(fs.readdir);
 
 // task 4
 const extractFileDataNames = ["extract", "extractFileData", "printFileData", "getFileData"];
-const extractFileData = (fileName) => {
+export const extractFileData = (fileName) => {
   const reader = fs.createReadStream(fileName);
   reader.pipe(process.stdout);
   reader.on('end', () => console.info('End of file'.blue))
@@ -18,7 +21,7 @@ const extractFileData = (fileName) => {
 
 // task 5
 const inputOutputNames = ["inputOutput", "input-output", "io"];
-const inputOutput = () => {
+export const inputOutput = () => {
   process.stdin
     .pipe(through2(function (chunk, enc, cb) {
       const str = chunk.toString();
@@ -31,8 +34,11 @@ const inputOutput = () => {
 
 // task 6
 const csvToJSONConsoleNames = ["csvToJSONConsole", "csvToJSONOut", "csvJSON"];
-const csvToJSONOut = (fileName) => {
-  if (path.extname(fileName).toLowerCase() !== '.csv') return ;
+export const csvToJSONOut = (fileName) => {
+  const extName = path.extname(fileName);
+  if (extName.toLowerCase() !== config.exts.csv) {
+    return ;
+  }
   fs.createReadStream(fileName)
     .pipe(through2(function (chunk, enc, cb) {
       const str = chunk.toString();
@@ -44,10 +50,12 @@ const csvToJSONOut = (fileName) => {
 }
 
 // task 7
-const csvToJSONFileNames = ["csvToJSONFile", , "csvJSONFile"];
-const csvToJSONFile = (fileName) => {
+const csvToJSONFileNames = ["csvToJSONFile", "csvJSONFile"];
+export const csvToJSONFile = (fileName) => {
   const extName = path.extname(fileName);
-  if (extName.toLowerCase() !== '.csv') return ;
+  if (extName.toLowerCase() !== config.exts.csv) {
+    return ;
+  }
 
   const baseName = path.basename(fileName, extName);
   const pathName = path.dirname(fileName);
@@ -67,23 +75,20 @@ const csvToJSONFile = (fileName) => {
 }
 
 // task 8
-const readdir = promisify(fs.readdir);
 const cssBundleNames = ["cssBundle", "cssBundler", "css", "bundler"];
-const cssBundle = (dirname) => {
-  const cssUrl = "https://www.epam.com/etc/clientlibs/foundation/main.min.fc69c13add6eae57cd247a91c7e26a15.css";
+export const cssBundle = (dirname) => {
   readdir(dirname)
     .then((files) => {
       if (!files || (files instanceof Array && files.length === 0)) {
-        console.info("No .css files to bundle".bgRed.white)
+        console.info("No .css files to bundle".bgRed.white);
         return ;
       }
-
       const writer = fs.createWriteStream(`${dirname}/bundle.css`, { flags: "a"});
       files.map((file) => {
-        if (path.extname(file).toLowerCase() === ".css" && file !== "bundle.css") {
+        if (path.extname(file).toLowerCase() === config.exts.css && file !== streamsCssBundleFile) {
           fs.createReadStream(`${dirname}/${file}`)
             .pipe(through2(function(chunk, enc, cb) {
-              const css = chunk.toString() + "\n";
+              const css = `${chunk.toString()}\n`;
               this.push(css);
               cb();
             }))
@@ -93,29 +98,22 @@ const cssBundle = (dirname) => {
 
       const httpWriter = fs.createWriteStream(`${dirname}/bundle.css`, { flags: "a"});
       httpWriter.on("finish", () => console.log("Closing writing".bgRed.white))
-      https.get(cssUrl, (res) => {
+      https.get(streamsCssUrl, (res) => {
         res.pipe(httpWriter);
       });
     })
     .catch(err => console.log(err));
 }
 
-const helpText = ("This tool will help you to run some stream utilities.\n" + 
-                "Just provide next options when calling a file to run your operation:\n\n").cyan +
-                ("--action, -a       Put here a method you want to call\n" + 
-                "--file, -f         Provide here a file you want to be operated with action\n" + 
-                "--path, -p         For CSS bundling only; path to CSS files to concatenate\n" + 
-                "--help, -h         Call me for help =)\n").rainbow;
-
 const args = minimist(process.argv.slice(2), config.streamsMinimistOpts);
 
 const runStreamTasks = (cliArgs) => {
   const argsKeys = Object.keys(cliArgs);
-  argsKeys.forEach((arg, i) => {
-    if (i === 1 && (arg === 'h' || arg === 'help')) {
-      console.info(helpText);
-    }
-  });
+  const indexOfHelp = argsKeys.findIndex(el => el === 'h' || el === 'help');
+
+  if (argsKeys.includes('h') && indexOfHelp === 1) {
+    console.info(streamsHelpText.bgBlue.white);
+  }
   
   if (argsKeys.length < 2) {
     console.info('No args found. If you donno what to do, add a "-h" or "--help" flag');
