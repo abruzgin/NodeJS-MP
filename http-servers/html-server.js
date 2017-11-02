1273dbdad6d59a6e861239dbaec98e9ec1b8c379
Aliaksandr Bruzgin <Aliaksandr_Bruzgin@epam.com>
2017-11-02 11:27:36 +0300

    fixes in html-server

--- http-servers/html-server.js
+++ http-servers/html-server.js
@@ -8,21 +8,13 @@ http.createServer()
     res.writeHead(200, {
       "Content-Type": "text/html"
     });
-
-    // read file with fs.readFileSync
-    // const html = fs.readFileSync("./index.html")
-    //   .toString()
-    //   .replace('{message}', 'Hello World!');
-    // res.end(html);
-
-    // read file with readable stream
     const readable = fs.createReadStream("./index.html");
     readable
       .pipe(through2(function(chunk, enc, cb) {
         const str = chunk
           .toString()
           .replace('{message}', 'Hello World!');
-        this.push(replaced);
+        this.push(str);
         cb();
       }))
       .pipe(res)
