382ddf1b407a4892d72b6c38630671b408dfdc4a
Aliaksandr Bruzgin <Aliaksandr_Bruzgin@epam.com>
2017-11-17 10:32:04 +0300

    Changed DB object assignment

--- expressServer/config/db.js
+++ expressServer/config/db.js
@@ -6,14 +6,13 @@ import { dbConfig } from './';
 const dbEnv = dbConfig[process.env.SQL_DB_ENV] || dbConfig.development;
 const { dialect, username, host, port, password, database } = dbEnv;
 
-const db = {};
 const sequelize = new Sequelize(`${dialect}://${username}:${password}@${host}:${port}/${database}`);
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-db.users = users(sequelize, Sequelize);
-db.products = products(sequelize, Sequelize);
+const db = {
+  sequelize,
+  Sequelize,
+  users: users(sequelize, Sequelize),
+  products: products(sequelize, Sequelize)
+};
 
 // provide the associations if necessary
 
